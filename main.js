e4285ab72786029c7d5789b13c024863a6e0cce0
quanber123 <quanber123@icloud.com>
2023-02-07 00:27:07 +0700

    changed

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 const accordionQuestion = document.querySelectorAll(".accordion-question");
 accordionQuestion.forEach((question) => {
-    question.addEventListener('click', () => {
-        if (question.parentNode.classList.contains('active')) {
-            question.parentNode.classList.remove('active');
-        } else {
-            question.parentNode.classList.add('active');
-        }
-    })
+        question.addEventListener('click', () => {
+            if (question.parentNode.classList.contains('active')) {
+                question.parentNode.classList.remove('active');
+            } else {
+                question.parentNode.classList.add('active');
+            }
+        })
 })
